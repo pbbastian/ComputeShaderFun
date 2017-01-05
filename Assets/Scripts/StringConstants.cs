125c91c52de65729b4ac5d88dd021be954c7b4cd
Peter Bay Bastian <peter.bastian@gmail.com>
2017-01-05 19:50:55 +0100

    Initial commit

---
+++ Assets/Scripts/StringConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+public static class StringConstants
+{
+    public static class MyFirstCompute
+    {
+        public static string csMain = "CSMain";
+        public static string result = "Result";
+    }
+}
