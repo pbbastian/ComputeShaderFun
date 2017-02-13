e20ac7fc30c2a43e691d4ce2c72ae41c9904d2a2
Peter Bay Bastian <peter.bastian@gmail.com>
2017-02-13 12:56:50 +0100

    ScanProgramTest is now data-driven (previously it was also kinda, but didnt use the facilities provided by NUnit to do so)

---
+++ Assets/RayTracer/Editor/Tests/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace RayTracer.Editor.Tests
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<TestCaseData> AsNamedTestCase<T>(this IEnumerable<T> enumerable, string name)
+        {
+            return enumerable.Select(x => new TestCaseData(x).SetName(string.Format("{0} ({1})", name, x)));
+        }
+    }
+}
--- Assets/RayTracer/Editor/Tests/ScanProgramTest.cs
+++ Assets/RayTracer/Editor/Tests/ScanProgramTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using RayTracer.Runtime.ShaderPrograms;
 using UnityEngine;
@@ -7,15 +8,45 @@ namespace RayTracer.Editor.Tests
 {
     public class ScanProgramTest
     {
-        private void SingleThreadGroup(int count, WarpSize warpSize)
+        public struct TestData
         {
-            var input = Enumerable.Range(24, count).Select(x => x + 1).ToArray();
+            public int count;
+            public WarpSize warpSize;
+
+            public override string ToString()
+            {
+                return string.Format("Count={0}, WarpSize={1}", count, (int) warpSize);
+            }
+        }
+
+        public static IEnumerable<TestCaseData> testDatas
+        {
+            get
+            {
+                var countMismatch =
+                    new[] {WarpSize.Warp16, WarpSize.Warp32, WarpSize.Warp64}
+                    .Select(warpSize => new TestData {count = 10, warpSize = warpSize})
+                    .AsNamedTestCase("Count mismatch");
+
+                var countMatch =
+                    new[] {WarpSize.Warp32, WarpSize.Warp64}.Select((warpSize) => new TestData {count = 1024, warpSize = warpSize})
+                    .Concat(new[] {new TestData {count = 256, warpSize = WarpSize.Warp16}})
+                    .AsNamedTestCase("Count match");
+
+                return countMismatch.Concat(countMatch);
+            }
+        }
+
+        [TestCaseSource("testDatas")]
+        public void VerifyOutput(TestData data)
+        {
+            var input = Enumerable.Range(24, data.count).Select(x => x + 1).ToArray();
             var output = new int[input.Length];
             var expected = new int[input.Length];
             for (var i = 1; i < input.Length; i++)
                 expected[i] = input.Take(i).Sum();
 
-            var scanProgram = new ScanProgram(warpSize);
+            var scanProgram = new ScanProgram(data.warpSize);
             using (var inputBuffer = new ComputeBuffer(input.Length, sizeof(float)))
             using (var dummyBuffer = new ComputeBuffer(1, 4))
             {
@@ -32,40 +63,40 @@ namespace RayTracer.Editor.Tests
             }
         }
 
-        [Test]
-        public void CountMatch_Warp16()
-        {
-            SingleThreadGroup(256, WarpSize.Warp16);
-        }
+        //[Test]
+        //public void CountMatch_Warp16()
+        //{
+        //    SingleThreadGroup(256, WarpSize.Warp16);
+        //}
 
-        [Test]
-        public void CountMatch_Warp32()
-        {
-            SingleThreadGroup(1024, WarpSize.Warp32);
-        }
+        //[Test]
+        //public void CountMatch_Warp32()
+        //{
+        //    SingleThreadGroup(1024, WarpSize.Warp32);
+        //}
 
-        [Test]
-        public void CountMatch_Warp64()
-        {
-            SingleThreadGroup(1024, WarpSize.Warp64);
-        }
+        //[Test]
+        //public void CountMatch_Warp64()
+        //{
+        //    SingleThreadGroup(1024, WarpSize.Warp64);
+        //}
 
-        [Test]
-        public void CountMismatch_Warp16()
-        {
-            SingleThreadGroup(10, WarpSize.Warp16);
-        }
+        //[Test]
+        //public void CountMismatch_Warp16()
+        //{
+        //    SingleThreadGroup(10, WarpSize.Warp16);
+        //}
 
-        [Test]
-        public void CountMismatch_Warp32()
-        {
-            SingleThreadGroup(10, WarpSize.Warp32);
-        }
+        //[Test]
+        //public void CountMismatch_Warp32()
+        //{
+        //    SingleThreadGroup(10, WarpSize.Warp32);
+        //}
 
-        [Test]
-        public void CountMismatch_Warp64()
-        {
-            SingleThreadGroup(10, WarpSize.Warp64);
-        }
+        //[Test]
+        //public void CountMismatch_Warp64()
+        //{
+        //    SingleThreadGroup(10, WarpSize.Warp64);
+        //}
     }
 }
