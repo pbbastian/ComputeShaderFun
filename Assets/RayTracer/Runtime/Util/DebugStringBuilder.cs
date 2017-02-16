19f54b907223b56b62bb2b797606e4cb7cdd85fd
Peter Bay Bastian <peter.bastian@gmail.com>
2017-02-16 10:37:39 +0100

    Debug utilities

---
+++ Assets/RayTracer/Runtime/Util/DebugStringBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RayTracer.Runtime.Util;
+
+namespace Assets.RayTracer.Runtime.Util
+{
+    public class DebugStringBuilder : IEnumerable<string>
+    {
+        private List<string> m_Items = new List<string>();
+
+        public void Add(string name, object value, string valueFormat = "{1}")
+        {
+            m_Items.Add(value.FormatDebug(name, valueFormat));
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return m_Items.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return StringDebug.Join(m_Items.ToArray());
+        }
+    }
+}
---
+++ Assets/RayTracer/Runtime/Util/StringDebug.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace RayTracer.Runtime.Util
+{
+    public static class StringDebug
+    {
+        public static string FormatDebug(this object arg, string name, string argFormat = "{1}")
+        {
+            return string.Format("{0}=" + argFormat, name, arg);
+        }
+
+        public static string Join(params string[] strings)
+        {
+            return string.Join(", ", strings);
+        }
+    }
+}
