f7e31fac3dee12c9f43bd18683cce96e1e053b4f
Peter Bay Bastian <peter.bastian@gmail.com>
2017-02-20 20:41:22 +0100

    Sequence shader program (fills a buffer with a sequence starting from 0)

---
+++ Assets/RayTracer/Runtime/ShaderPrograms/SequenceProgram.cs
@@ -0,0 +1,28 @@
+﻿using RayTracer.Runtime.Util;
+using UnityEngine;
+
+namespace RayTracer.Runtime.ShaderPrograms
+{
+    public class SequenceProgram
+    {
+        private static readonly int s_BufferId = Shader.PropertyToID("g_Buffer");
+        private int m_KernelIndex;
+        private ComputeShader m_Shader;
+        private int m_SizeX;
+
+        public SequenceProgram()
+        {
+            m_Shader = Resources.Load<ComputeShader>("Shaders/Sequence");
+            m_KernelIndex = m_Shader.FindKernel("Sequence");
+            uint x, y, z;
+            m_Shader.GetKernelThreadGroupSizes(m_KernelIndex, out x, out y, out z);
+            m_SizeX = (int)x;
+        }
+
+        public void Dispatch(int count, ComputeBuffer buffer)
+        {
+            m_Shader.SetBuffer(m_KernelIndex, s_BufferId, buffer);
+            m_Shader.Dispatch(m_KernelIndex, count.CeilDiv(m_SizeX), 1, 1);
+        }
+    }
+}
